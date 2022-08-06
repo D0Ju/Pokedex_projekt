66085108197ed376e070ab230140a5750c035872
Dominik Jurusic <dominik.jurusic@gmail.com>
2022-08-07 01:55:05 +0200

    cssdodan

--- Api_fetch.js
+++ Api_fetch.js
@@ -7,20 +7,46 @@ async function getData(){
         const GlavniDiv  = document.getElementById('PokedexContainerId');
         const div =  document.createElement('div');
         div.classList.add("item");
-        div.innerHTML = Pokemon.name;   
+        div.innerHTML = Pokemon.name;
         GlavniDiv.append(div);
+        getImage();
     })
 }
 
+// const api_url_slike = 'https://pokeapi.co/api/v2/pokemon';
+
+// async function getPicture(){
+//     const response = await fetch(api_url_slike);
+//     const data = await response.json();
+//     data.results.forEach(Slika => {
+
+//         const img =  document.createElement('img');
+//         img.innerHTML = ;  
+//         GlavniDiv.append(div);
+//     })
+// }
+
+
+//dohvacanje slike 
+const imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/2.png";
+
+function getImage(){
+fetch(imageUrl)
+  //                         vvvv
+  .then(response => response.blob())
+  .then(imageBlob => {
+      // Then create a local URL for that image and print it 
+      const imageObjectURL = URL.createObjectURL(imageBlob);
+      console.log(imageObjectURL);
+  });
+}
    
 
 
 getData();
 
 
+//mozda ce bit korisno
+//${list.children.length + 1}
 
-
-// fetch('https://pokeapi.co/api/v2/pokemon/ditto
-//     .then(res => res.json())
-//     .then (data => console.log(data))
-//     .then(error => console.log('ERROR'))
+//mozda da probam napravit svaku pokemon karticu kao svoj zasebni container te u njemu imam 3 diva za img button i ime
